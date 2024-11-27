401af03b65eca8139f4b9abc08040d1993448134
Augusto Brito <121031260+wgutto@users.noreply.github.com>
2024-11-26 21:58:24 -0300

    new wx

---
+++ aula011/ex008.js
@@ -0,0 +1,13 @@
+var vel = 60.2
+var velmax = 60
+
+console.log(`A velocidade máxima permitida nessa via é ${velmax}Km/h`)
+console.log(`A velocidade do seu carro é ${vel}Km/h.`)
+
+if(vel > 60) {
+    console.log('Você foi multado!')
+}else {
+    console.log('Você está na velocidade normal.')
+}
+
+console.log(`Dirija sempre com o cinto de segurança, boa viagem.`)
