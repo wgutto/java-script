f8aae8076d32cf5704258167b33ac5f9b76992cb
Augusto Brito <121031260+wgutto@users.noreply.github.com>
2024-11-30 15:09:16 -0300

    new att

--- aula014ex/ex016/js/script.js
+++ aula014ex/ex016/js/script.js
@@ -1,32 +1,35 @@
-var botao = document.querySelector('main div#botao')
+var botao = document.querySelector('main div #botao')
 
 botao.addEventListener('click', contar)
 
 function contar() {
     var inicio = document.querySelector('main div#infos #inicio')
-    var fim = document.querySelector('main div#infos #fim')
+    var fim =   document.querySelector('main div#infos #fim')
     var passo = document.querySelector('main div#infos #passo')
     var resultado = document.querySelector('main div#resultado')
 
     if(inicio.value == 0 || fim.value == 0 || passo.value == 0) {
-        alert('Errro')
+        resultado.innerHTML = `[ERRO] Insira os dados.`
     } else {
         var i = Number(inicio.value)
         var f = Number(fim.value)
         var p = Number(passo.value)
+
         resultado.innerHTML = `Contando: `
+
         if(p < 0) {
-            resultado.innerHTML = `[ERRO] São aceito apenas números positivos, será considerado o número 1. <br>`
+            resultado.innerHTML = `[ERRO] Valor de passo inválido. Considerando valor 1. <br>`
             p = 1
         }
+
         if(i < f) {
-            for(var contador = i; contador <= f; contador += p)
-            resultado.innerHTML += ` ${contador} \u{1F449}`
+            for(var contador = i; contador <= f; contador += p) {
+                resultado.innerHTML += `${contador} `
+            }
         } else {
             for(var contador = i; contador >= f; contador -= p) {
-                resultado.innerHTML += ` ${contador} \u{1F449}`
+                resultado.innerHTML += `${contador} `
             }
         }
-        resultado.innerHTML += `\u{1F3F4}`
     }
 }
